7f6d3a34854b5b862aaf6947c2682e2c03b66d1f
Arthur Malmann Becker <arthmalbeck@gmail.com>
2022-10-10 23:50:31 -0300

    feature: create load component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="spin-load"/>
+    </div>
+  );
+};
+
+export default Loading;
