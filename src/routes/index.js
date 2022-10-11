fc89d662c40619d6dc5c95a13a3e3b04f8b68095
Arthur Malmann Becker <arthmalbeck@gmail.com>
2022-10-10 23:49:57 -0300

    improvement: centralize routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBarApp from "../components/NavBar";
+import { GAME_ROUTE, PLAY_ROUTE } from "../constants/routes";
 
-import Dashboard from "../pages/public/dashboard";
-import GameProfile from "../pages/public/gameProfile";
-import PlayGame from "../pages/public/playGame";
+import GameLibrary from "../pages/gameLibrary";
+import GameProfile from "../pages/gameProfile";
+import PlayGame from "../pages/playGame";
 
 const RoutesApp = () => {
   return (
@@ -12,9 +13,9 @@ const RoutesApp = () => {
       <NavBarApp/>
       <Fragment>
         <Routes>
-          <Route exact path="/play/:id" element={<PlayGame />} />
-          <Route exact path="/game/:id" element={<GameProfile />} />
-          <Route path="*" element={<Dashboard />} />
+          <Route exact path={`${PLAY_ROUTE}/:id`} element={<PlayGame />} />
+          <Route exact path={`${GAME_ROUTE}/:id`} element={<GameProfile />} />
+          <Route path="*" element={<GameLibrary />} />
         </Routes>
       </Fragment>
     </BrowserRouter>
